d3c1436100ec86055fcd7f3bdd82bd7b4b157287
naveen <naveenkumar51@gmail.com>
2013-09-10 13:07:38 +0530

    Update import_products_hooks.rb

--- lib/import_products_hooks.rb
+++ lib/import_products_hooks.rb
@@ -1,6 +1,6 @@
-class ImportProductsHooks < Spree::ThemeSupport::HookListener
+class ImportProductsHooks #< Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :admin_tabs do
-   %(<%= tab(:product_imports) %>)
-  end
+  #insert_after :admin_tabs do
+  # %(<%#= tab(:product_imports) %>)
+  #end
 end
